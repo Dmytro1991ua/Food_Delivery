c6ab470819096413436ed21445c6189dbb3c009d
Dmytro1991ua <61331410+Dmytro1991ua@users.noreply.github.com>
2020-04-10 15:12:30 +0300

    Food__Delivery
    A website for Food Delivery

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//script (function) which is gonna help to open and close modal window on click
+function runModalWindow() {
+   const shoppingCart = document.querySelector(".shopping-cart");
+   const modal = document.querySelector(".modal");
+   const closeModal = document.querySelector(".modal__close");
+
+
+   shoppingCart.addEventListener("click", toggleModal);
+
+   closeModal.addEventListener("click", toggleModal);
+
+   function toggleModal() {
+      modal.classList.toggle("open-modal");
+   }
+}
+runModalWindow();
+
+//activate WOW.js
+new WOW().init({
+   mobile: false
+});
+
+
+
